fb519ded393c6c4d89cef59eea8353a806687138
edeotena <edeotena13@gmail.com>
2023-06-01 14:19:52 +0300

    fixes

--- src/ex02/day2_ex02.sql
+++ src/ex02/day2_ex02.sql
@@ -2,9 +2,9 @@ SELECT COALESCE(person.name, '-') person_name,
 	   visits.visit_date, 
 	   COALESCE(pizzeria.name, '-') pizzeria_name
 FROM (SELECT *
-			 FROM person_visits pv
-			 WHERE visit_date BETWEEN '2022-01-01' 
-	         AND '2022-01-03') visits
+	  FROM person_visits pv
+	  WHERE visit_date BETWEEN '2022-01-01' 
+	  AND '2022-01-03') visits
 RIGHT JOIN person ON person.id = visits.person_id
 FULL JOIN pizzeria ON pizzeria.id = visits.pizzeria_id
 ORDER BY person_name, visit_date, pizzeria_name;
--- src/ex10/day2_ex10.sql
+++ src/ex10/day2_ex10.sql
@@ -3,4 +3,5 @@ SELECT p1.name person_name1,
 	   p1.address common_address
 FROM person p1
 JOIN person p2 ON p1.address = p2.address AND 
-				  p1.id > p2.id;
+				  p1.id > p2.id
+ORDER BY person_name1, person_name2, common_address;
