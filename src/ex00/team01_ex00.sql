c4a8142a1da15a6fa2be18f4e5bb0a1d564d75fc
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-06-09 20:01:50 +0300

    trying to make first request

--- src/ex00/team01_ex00.sql
+++ src/ex00/team01_ex00.sql
@@ -1,1 +1,34 @@
--- TODO: sdelat' vse
+WITH last_rates AS (
+    SELECT DISTINCT ON (id) id, name, rate_to_usd
+    FROM currency
+    ORDER BY id, updated DESC
+)
+SELECT COALESCE(u.name, 'not defined') AS name,
+       COALESCE(u.lastname, 'not defined') AS lastname,
+       b.type AS type,
+       SUM(b.money) AS volume,
+       COALESCE(MAX(lr.name), 'not defined') AS currency_name,
+       COALESCE(MAX(lr.rate_to_usd), 1) AS last_rate_to_usd,
+       SUM(b.money) * COALESCE(MAX(lr.rate_to_usd), 1) AS total_volume_in_usd
+FROM "user" u
+FULL JOIN balance b ON u.id = b.user_id
+LEFT JOIN last_rates lr ON lr.id = b.currency_id
+GROUP BY u.name, u.lastname, b.type
+ORDER BY u.name DESC, u.lastname ASC, b.type ASC;
+
+-- WITH last_rates AS (
+--     SELECT DISTINCT ON (id) id, name, rate_to_usd
+--     FROM currency
+--     ORDER BY id, updated DESC
+-- )
+-- SELECT COALESCE(u.name, 'not defined') AS name,
+--        COALESCE(u.lastname, 'not defined') AS lastname,
+--        b.type AS type,
+--        b.money AS volume,
+--        COALESCE(lr.name, 'not defined') AS currency_name,
+--        COALESCE(lr.rate_to_usd, 1) AS last_rate_to_usd,
+--        b.money * COALESCE(lr.rate_to_usd, 1) AS total_volume_in_usd
+-- FROM "user" u
+-- FULL JOIN balance b on u.id = b.user_id
+-- LEFT JOIN last_rates lr on lr.id = b.currency_id
+-- ORDER BY u.name DESC, u.lastname, b.type;
